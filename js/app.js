fc58a5cc409037d3269096558d003a956aaffd42
Ice-CubeXQ <dealplug72@gmail.com>
2023-10-14 20:03:01 +0300

    a new block has been added and the header has been changed

--- js/app.js
+++ js/app.js
@@ -1,26 +1,48 @@
 (() => {
   "use strict";
-  let e = !0,
-    t = (t = 500) => {
-      let n = document.querySelector("body");
-      if (e) {
+  let e = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+      return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+      return (
+        e.Android() || e.BlackBerry() || e.iOS() || e.Opera() || e.Windows()
+      );
+    },
+  };
+  let t = !0,
+    r = (e = 500) => {
+      let r = document.querySelector("body");
+      if (t) {
         let o = document.querySelectorAll("[data-lp]");
         setTimeout(() => {
           for (let e = 0; e < o.length; e++) {
             o[e].style.paddingRight = "0px";
           }
-          (n.style.paddingRight = "0px"),
+          (r.style.paddingRight = "0px"),
             document.documentElement.classList.remove("lock");
-        }, t),
-          (e = !1),
+        }, e),
+          (t = !1),
           setTimeout(function () {
-            e = !0;
-          }, t);
+            t = !0;
+          }, e);
       }
     },
-    n = (t = 500) => {
-      let n = document.querySelector("body");
-      if (e) {
+    o = (e = 500) => {
+      let r = document.querySelector("body");
+      if (t) {
         let o = document.querySelectorAll("[data-lp]");
         for (let e = 0; e < o.length; e++) {
           o[e].style.paddingRight =
@@ -28,20 +50,95 @@
             document.querySelector(".wrapper").offsetWidth +
             "px";
         }
-        (n.style.paddingRight =
+        (r.style.paddingRight =
           window.innerWidth -
           document.querySelector(".wrapper").offsetWidth +
           "px"),
           document.documentElement.classList.add("lock"),
-          (e = !1),
+          (t = !1),
           setTimeout(function () {
-            e = !0;
-          }, t);
+            t = !0;
+          }, e);
       }
     };
-  let o = !1;
+  const l = { inputMaskModule: null, selectModule: null };
+  let a = {
+    getErrors(e) {
+      let t = 0,
+        r = e.querySelectorAll("*[data-required]");
+      return (
+        r.length &&
+          r.forEach((e) => {
+            (null === e.offsetParent && "SELECT" !== e.tagName) ||
+              e.disabled ||
+              (t += this.validateInput(e));
+          }),
+        t
+      );
+    },
+    validateInput(e) {
+      let t = 0;
+      return (
+        "email" === e.dataset.required
+          ? ((e.value = e.value.replace(" ", "")),
+            this.emailTest(e) ? (this.addError(e), t++) : this.removeError(e))
+          : ("checkbox" !== e.type || e.checked) && e.value
+          ? this.removeError(e)
+          : (this.addError(e), t++),
+        t
+      );
+    },
+    addError(e) {
+      e.classList.add("_form-error"),
+        e.parentElement.classList.add("_form-error");
+      let t = e.parentElement.querySelector(".form__error");
+      t && e.parentElement.removeChild(t),
+        e.dataset.error &&
+          e.parentElement.insertAdjacentHTML(
+            "beforeend",
+            `<div class="form__error">${e.dataset.error}</div>`,
+          );
+    },
+    removeError(e) {
+      e.classList.remove("_form-error"),
+        e.parentElement.classList.remove("_form-error"),
+        e.parentElement.querySelector(".form__error") &&
+          e.parentElement.removeChild(
+            e.parentElement.querySelector(".form__error"),
+          );
+    },
+    formClean(e) {
+      e.reset(),
+        setTimeout(() => {
+          let t = e.querySelectorAll("input,textarea");
+          for (let e = 0; e < t.length; e++) {
+            const r = t[e];
+            r.parentElement.classList.remove("_form-focus"),
+              r.classList.remove("_form-focus"),
+              a.removeError(r),
+              (r.value = r.dataset.placeholder);
+          }
+          let r = e.querySelectorAll(".checkbox__input");
+          if (r.length > 0)
+            for (let e = 0; e < r.length; e++) {
+              r[e].checked = !1;
+            }
+          if (l.selectModule) {
+            let t = e.querySelectorAll(".select");
+            if (t.length)
+              for (let e = 0; e < t.length; e++) {
+                const r = t[e].querySelector("select");
+                l.selectModule.selectBuild(r);
+              }
+          }
+        }, 0);
+    },
+    emailTest: (e) =>
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(e.value),
+  };
+  let n = !1;
   setTimeout(() => {
-    if (o) {
+    if (n) {
       let e = new Event("windowScroll");
       window.addEventListener("scroll", function (t) {
         document.dispatchEvent(e);
@@ -61,15 +158,73 @@
       let t = !0 === e ? "webp" : "no-webp";
       document.documentElement.classList.add(t);
     }),
+    e.any() && document.documentElement.classList.add("touch"),
     (function () {
-      let o = document.querySelector(".icon-menu");
-      o &&
-        o.addEventListener("click", function (o) {
-          e &&
+      let e = document.querySelector(".icon-menu");
+      e &&
+        e.addEventListener("click", function (e) {
+          t &&
             (((e = 500) => {
-              document.documentElement.classList.contains("lock") ? t(e) : n(e);
+              document.documentElement.classList.contains("lock") ? r(e) : o(e);
             })(),
             document.documentElement.classList.toggle("menu-open"));
         });
+    })(),
+    (function () {
+      const e = document.querySelectorAll(
+        "input[placeholder],textarea[placeholder]",
+      );
+      e.length &&
+        e.forEach((e) => {
+          e.dataset.placeholder = e.placeholder;
+        }),
+        document.body.addEventListener("focusin", function (e) {
+          const t = e.target;
+          ("INPUT" !== t.tagName && "TEXTAREA" !== t.tagName) ||
+            (t.dataset.placeholder && (t.placeholder = ""),
+            t.classList.add("_form-focus"),
+            t.parentElement.classList.add("_form-focus"),
+            a.removeError(t));
+        }),
+        document.body.addEventListener("focusout", function (e) {
+          const t = e.target;
+          ("INPUT" !== t.tagName && "TEXTAREA" !== t.tagName) ||
+            (t.dataset.placeholder && (t.placeholder = t.dataset.placeholder),
+            t.classList.remove("_form-focus"),
+            t.parentElement.classList.remove("_form-focus"),
+            t.hasAttribute("data-validate") && a.validateInput(t));
+        });
+    })(),
+    (function () {
+      n = !0;
+      const e = document.querySelector("header.header"),
+        t = e.hasAttribute("data-scroll-show"),
+        r = e.dataset.scrollShow ? e.dataset.scrollShow : 500,
+        o = e.dataset.scroll ? e.dataset.scroll : 1;
+      let l,
+        a = 0;
+      document.addEventListener("windowScroll", function (n) {
+        const s = window.scrollY;
+        clearTimeout(l),
+          s >= o
+            ? (!e.classList.contains("_header-scroll") &&
+                e.classList.add("_header-scroll"),
+              t &&
+                (s > a
+                  ? e.classList.contains("_header-show") &&
+                    e.classList.remove("_header-show")
+                  : !e.classList.contains("_header-show") &&
+                    e.classList.add("_header-show"),
+                (l = setTimeout(() => {
+                  !e.classList.contains("_header-show") &&
+                    e.classList.add("_header-show");
+                }, r))))
+            : (e.classList.contains("_header-scroll") &&
+                e.classList.remove("_header-scroll"),
+              t &&
+                e.classList.contains("_header-show") &&
+                e.classList.remove("_header-show")),
+          (a = s <= 0 ? 0 : s);
+      });
     })();
 })();
